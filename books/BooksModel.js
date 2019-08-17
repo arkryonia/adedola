3a99ff58f7f73ab3b7f04b8b5da77b7b8156188b
Hodonou SOUNTON <sounton@gmail.com>
2019-08-18 00:27:09 +0100

    fixed:connected to mongo atlas

---
+++ books/BooksModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+mongoose.model("Book", {
+  title: {
+    type: String,
+    require: true
+  },
+  author: {
+    type: String,
+    require: true
+  },
+  numberPages: {
+    type: Number,
+    require: true
+  },
+  publisher: {
+    type: String,
+    require: true
+  }
+})
--- books/books.js
+++ books/books.js
@@ -1,9 +1,34 @@
-// Load express framework
-const express = require("express")
-const app = express()
+// Load express framework --> sounton@gmail.com : q3auLZo6HJ90D6sP
+const dotenv     = require('dotenv')
+const express    = require("express")
+const bodyParser = require("body-parser")
+
+// Fired up detenv
+dotenv.config()
+
+// Instanciate expressjs
+const app        = express()
+
+// Express extensions
+app.use(bodyParser.json())
+
+// Load mongoose
+const mongoose = require("mongoose")
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Database is fired up!")
+}, (err) => {
+    console.log(err)
+})
 
 app.get('/', (req, res) => {
     res.send("This is our main endpoint!")
+})
+
+app.post('/book', (req, res) => {
+    console.log(req.body)
+    res.send("Testing our book route!")
 })
 
 app.listen(4545, () => {
